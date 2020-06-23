75754b707da7d2f65bb47535f9e935f09a0db65f
aSilas <silastj@hotmail.com>
2020-06-22 23:57:09 -0300

    Funções literais

--- js/script.js
+++ js/script.js
@@ -3,4 +3,32 @@
 // VAR ESTÁ FORA DO OBJETO
 //PROPRIEDADES ESTÁD DENTRO DO OBJETO
 
-//
+//LITERAL
+function somar(n1, n2){
+    return n1 + n2;
+}
+
+var soma = somar(10,20)
+    console.log('RES: ' + parseInt(soma));
+console.log(typeof(soma));
+
+//ADD PROPRIEDADE NA FUNÇÃO
+
+somar.frase = 'Aqui está a frase';
+console.log(somar.frase);
+
+// FUNÇÃO ANOMINA
+var teste = function(){
+    console.log('teste');
+}
+
+teste();
+
+//FUNÇÃO PASSADA POR PARAMETRO
+var parametro = function(f){
+    f();
+};
+
+parametro(function(){
+    console.log('função passada por parametro');
+});
