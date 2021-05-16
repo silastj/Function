a042265ebc57fd254cf83c21a584ce0324f546b7
aSilas <silastj@hotmail.com>
2021-05-15 23:21:15 -0300

    Aulas de string

--- array/script.js
+++ array/script.js
@@ -264,3 +264,36 @@ function avarege(){
 console.log(avarege(10,20))
 
 //ES5 Antigamente
+function ss(n1,n2){
+    return n1 + n2
+}
+//Array.prototype()
+
+ss(10,12)
+ss.call(null, 10, 12) //Passa o primeiro parametro null e depois os valores
+ss.apply(null, [1,2,3,4,5]) // Passa o primeiro parametro null e depois um array
+
+//Strings
+
+//replace()  ELe troca o primeiro parametro, para o segundo parametro
+const re = 'O maior do mundo Deus!, maior do mundo Jesus!'
+console.log(re)
+console.log(re.replace('a','i'))
+
+//replaceAll() Ele troca todos que estiver no primeiro parametro para o segundo parametro
+console.log(re.replaceAll('a', 'i'))
+// Com Regex, podemos usar para dar suporte aos demais navegadores
+console.log('regex: '+re.replace(/i/g, "o"))
+
+//indexOf() Posição da palavra string buscando do lado esquerdo para o direito,
+//caso não existe retorna -1
+console.log('indexOf: '+re.indexOf('Deus'))
+
+//lastIndex()  Procura a mesma coisa que indexOf, mas do ultimo para o primeiro,
+//caso não existe retorna -1
+console.log(('lastIndex: '+re.lastIndexOf('Jesus')))
+
+//includes() Ele retorna se existe algo, passado no parametro
+console.log('includes: '+re.includes('Deus'))
+
+
